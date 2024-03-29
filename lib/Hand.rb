bc71031fcebeba97d376f93432e11b2a2cfe2a1b
Benjamin Siler <bengsiler@gmail.com>
2024-03-29 17:36:04 -0500

    Added logic in Hand.determine_strength to determine a flush

--- lib/Hand.rb
+++ lib/Hand.rb
@@ -3,4 +3,18 @@ class Hand
   def initialize()
     @cards = []
   end
+  def determine_strength()
+    same_suit = true
+    suit = @cards[0].suit
+    @cards.each do |card|
+      if card.suit != suit
+        same_suit = false
+      end
+    end
+    if same_suit
+      return "Flush"
+    else
+      return "Not Flush"
+    end
+  end
 end
