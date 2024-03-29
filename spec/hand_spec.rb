9e7b0d382693a3a54bce482c0567b9b0818ede47
Benjamin Siler <bengsiler@gmail.com>
2024-03-29 18:30:25 -0500

    Reorganized hand_spec.rb

--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -1,16 +1,29 @@
 require_relative '../lib/hand'
 require 'pp'
 RSpec.describe Hand do
-  let(:hand) { Hand.new() }
+  let(:empty_hand) { Hand.new() }
+  let(:flush_hand) { Hand.new([
+    Card.new(5, "Hearts"),
+    Card.new(8, "Hearts"),
+    Card.new(1, "Hearts"),
+    Card.new(3, "Hearts"),
+    Card.new(4, "Hearts")
+  ]) }
+  let(:straight_hand) { Hand.new([
+    Card.new(5, "Hearts"),
+    Card.new(6, "Hearts"),
+    Card.new(7, "Hearts"),
+    Card.new(8, "Hearts"),
+    Card.new(9, "Clubs")
+  ])}
   it "constructs" do
     hand = Hand.new()
   end
-  it "constructs with list of cards" do
-  end
   context "initialized" do
     context "empty" do
+      let(:hand) { Hand.new() }
       it "has no cards" do
-        expect(hand.cards.size).to eq(0)
+        expect(empty_hand.cards.size).to eq(0)
       end
       it "can receive a new card" do
         hand.cards << Card.new(7, "Hearts")
@@ -31,33 +44,21 @@ RSpec.describe Hand do
   describe ".determine_strength" do
     context "when a flush" do
       it "returns flush" do
-        flush = Hand.new()
-        flush.cards << Card.new(5, "Hearts")
-        flush.cards << Card.new(8, "Hearts")
-        flush.cards << Card.new(1, "Hearts")
-        flush.cards << Card.new(3, "Hearts")
-        flush.cards << Card.new(4, "Hearts")
-        expect(flush.determine_strength).to eq("Flush")
+        expect(flush_hand.determine_strength).to eq("Flush")
       end
     end
     context "when a straight" do
       it "returns straight" do
-        straight = Hand.new()
-        straight.cards << Card.new(5, "Hearts")
-        straight.cards << Card.new(6, "Hearts")
-        straight.cards << Card.new(7, "Hearts")
-        straight.cards << Card.new(8, "Hearts")
-        straight.cards << Card.new(9, "Clubs")
-        expect(straight.determine_strength).to eq("Straight")
+        expect(straight_hand.determine_strength).to eq("Straight")
       end
     end
   end
   describe ".same_suit" do
     it "returns true with two cards of same suit" do
-      expect(hand.same_suit([Card.new(1,"Clubs"), Card.new(8,"Clubs")])).to eq(true)
+      expect(empty_hand.same_suit([Card.new(1,"Clubs"), Card.new(8,"Clubs")])).to eq(true)
     end
     it "returns false with two cards not of same suit" do
-      expect(hand.same_suit([Card.new(1,"Clubs"), Card.new(8,"Hearts")])).to eq(false)
+      expect(empty_hand.same_suit([Card.new(1,"Clubs"), Card.new(8,"Hearts")])).to eq(false)
     end
   end
 end
