0799ed175079fe6fe2b26840c335674bca9e8cfc
Benjamin Siler <bengsiler@gmail.com>
2024-03-29 17:07:41 -0500

    Added test for 'Hand when initialized can receive a new card'

--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -1,5 +1,5 @@
 require_relative '../lib/hand'
-
+require 'pp'
 RSpec.describe Hand do
   let(:hand) { Hand.new() }
   it "constructs" do
@@ -8,6 +8,11 @@ RSpec.describe Hand do
   context "when initialized" do
     it "has no cards" do
       expect(hand.cards.size).to eq(0)
+    end
+    it "can receive a new card" do
+      hand.cards << Card.new(7, "Hearts")
+      expect(hand.cards.size).to eq(1)
+      pp hand.cards
     end
   end
 end
