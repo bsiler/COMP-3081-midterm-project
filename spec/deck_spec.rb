50ba44c0ca6fd90dce488c3fd3e48a1591270a16
Benjamin Siler <bengsiler@gmail.com>
2024-03-29 16:33:54 -0500

    Added test for 'Deck when initialized has cards' and put testing Deck in let setup

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -2,7 +2,13 @@ require_relative '../lib/deck'
 require_relative '../lib/card'
 
 RSpec.describe Deck do
+  let(:deck) { Deck.new() }
   it "constructs" do
     deck = Deck.new()
+  end
+  context "when initialized" do
+    it "has cards" do
+      deck.cards
+    end
   end
 end
