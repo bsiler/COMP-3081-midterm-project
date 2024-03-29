c4b23cd10651b13677719347204b6ee674dc9753
Benjamin Siler <bengsiler@gmail.com>
2024-03-29 18:12:16 -0500

    Refactored Hand.rb, added is_straight() class method

--- lib/Hand.rb
+++ lib/Hand.rb
@@ -7,19 +7,11 @@ class Hand
     if same_suit(@cards)
       return "Flush"
     end
-    is_straight = true
-    @cards.sort_by {|card| card.value}
-    last_value = @cards[0].value - 1
-    @cards.each do |card|
-      if card.value != last_value + 1
-        is_straight = false
-      end
-      last_value = card.value
-    end
-    if is_straight
+    if is_straight(@cards)
       return "Straight"
     end
   end
+
   def same_suit(cards)
     same_suit = true
     suit = cards[0].suit
@@ -29,5 +21,18 @@ class Hand
       end
     end
     return same_suit
+  end
+
+  def is_straight(cards)
+    is_straight = true
+    cards.sort_by {|card| card.value}
+    last_value = cards[0].value - 1
+    cards.each do |card|
+      if card.value != last_value + 1
+        is_straight = false
+      end
+      last_value = card.value
+    end
+    return is_straight
   end
 end
