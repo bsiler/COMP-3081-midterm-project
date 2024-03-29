0cd78fe40382cfd09230e32b05d1369de442ed7a
Benjamin Siler <bengsiler@gmail.com>
2024-03-29 17:01:35 -0500

    Added test for 'Hand when initialized has no cards'

--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -1,7 +1,13 @@
 require_relative '../lib/hand'
 
 RSpec.describe Hand do
+  let(:hand) { Hand.new() }
   it "constructs" do
     hand = Hand.new()
+  end
+  context "when initialized" do
+    it "has no cards" do
+      expect(hand.cards.size).to eq(0)
+    end
   end
 end
