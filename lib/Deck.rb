e230dcfe28170b96a643319bf8b3b6564adbb82f
Benjamin Siler <bengsiler@gmail.com>
2024-03-29 16:45:58 -0500

    Added code in Deck constructor to fill Deck with 52 Cards

--- lib/Deck.rb
+++ lib/Deck.rb
@@ -1,8 +1,14 @@
 require 'Card'
-
+require 'pp'
 class Deck
   attr_reader :cards
   def initialize()
     @cards = []
+    suits = ["Hearts", "Clubs", "Diamonds", "Spades"]
+    suits.each do |suit|
+      (1..13).each do |value|
+        @cards << Card.new(value, suit)
+      end
+    end
   end
 end
